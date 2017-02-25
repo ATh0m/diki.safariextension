cfa0fb70b94c362dbad5d11cb944f4a24bb40f75
Tomasz Nanowski <tomasz.nanowski@gmail.com>
2017-02-25 13:58:50 +0100

    Added first completed version

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# safari-diki
+# diki.safariextension
 Safari extension to lookup word in diki.pl dictionary
+
+![screen.png](screen.png)
+
+
+
+**Everything what you need to do is double click specific word. You can alse select that word and double press `d` key**
---
+++ script.js
@@ -0,0 +1,58 @@
+function lookup(query) {
+    var lookupView = document.getElementById('diki-lookup');
+
+    if (!lookupView) {
+        lookupView = document.createElement('object');
+
+        lookupView.id = "diki-lookup";
+        lookupView.type = "text/html";
+
+        document.body.appendChild(lookupView);
+    }
+
+    lookupView.style.display = 'block';
+
+    query = query.trim();
+    lookupView.data = "https://www.diki.pl/slownik-angielskiego?q=" + query;
+};
+
+function main() {
+    var delta = 500;
+    var lastKeyPressTime = 0;
+
+    document.addEventListener('keydown', function handleKeydown(e) {
+        if (e.keyCode != 68) { return; }
+
+        var currentKeyPressTime = new Date();
+        if (currentKeyPressTime - lastKeyPressTime <= delta) {
+            var selected = document.getSelection().toString();
+
+            lookup(selected);
+        }
+        
+        lastKeyPressTime = currentKeyPressTime;
+    }, false);
+    
+    document.addEventListener('dblclick', function handleDblclick(e) {
+        var target = e.target;
+        if (target.tagName === 'INPUT' && target.type === 'text') { return; }
+
+        var selected = document.getSelection().toString().trim();
+        if (selected === '') { return; }
+
+        lookup(selected);
+    }, false);
+
+    document.addEventListener('click', function handleClick(e) {
+        var target = e.target;
+
+
+        var lookupView = document.getElementById('diki-lookup');
+        if (target == lookupView) return;
+        if (target.parent == lookupView) return;
+
+        lookupView.style.display = 'none';
+    }, false);
+};
+
+main();
